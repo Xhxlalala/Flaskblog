e12dd58e0812e60b6304dffb14bc62c8e3b7feca
Xhxlalala <2216943676@qq.com>
2022-04-11 09:56:11 +0800

    commit code

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '66294800a89b651b6fb0293af3823f76'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = '您尚未登录，请先登录'
+login_manager.login_message_category = 'info'
 
 # routes.py先导入了app,所以route导入要在app后边
 from flaskblog import routes
