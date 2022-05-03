9fe1f02671e71e80de61014d9a3d3b9758712f9f
Xhxlalala <40828158+Xhxlalala@users.noreply.github.com>
2022-05-03 19:14:53 +0800

    Update

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = '66294800a89b651b6fb0293af3823f76'
+    SECRET_KEY = '###########################'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'xhx_mail@163.com'
-    MAIL_PASSWORD = 'RITTFFYSVMDUAXZI'
-    MAIL_DEFAULT_SENDER = "Xtar's blog <xhx_mail@163.com>"
+    MAIL_USERNAME = '邮箱：#######@163.com'
+    MAIL_PASSWORD = '授权码：#############'
+    MAIL_DEFAULT_SENDER = "Xtar's blog <#######@163.com>"
